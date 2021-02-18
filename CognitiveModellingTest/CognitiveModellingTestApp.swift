8d211e921330fd14eab1197c20f85856adecee54
Group 3 <h.b.r.van.de.langemheen@student.rug.nl>
2021-02-18 10:43:48 +0100

    Initial Commit

---
+++ CognitiveModellingTest/CognitiveModellingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CognitiveModellingTestApp.swift
+//  CognitiveModellingTest
+//
+//  Created by H.B.R. van de Langemheen on 18/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct CognitiveModellingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CognitiveModellingTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CognitiveModellingTest
+//
+//  Created by H.B.R. van de Langemheen on 18/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
