cbc3d99e116781eea42a08b1dcc143d2ee4d0d28
Group 3 <h.b.r.van.de.langemheen@student.rug.nl>
2021-02-18 11:31:08 +0100

    added hand

--- CognitiveModellingTest/PlayingFieldView.swift
+++ CognitiveModellingTest/PlayingFieldView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct PlayingFieldView: View {
-    let data = Array(1...45).map { "item \($0)" }
+    let data = Array(1...45).map { "\($0)" }
+    
+    let hand: Array<Color> = [Color.red, Color.green, Color.blue, Color.orange, Color.purple, Color.yellow, Color.pink, Color.black, Color.gray]
     
     let layout = [
         GridItem(.flexible(), spacing: 1),
@@ -19,34 +21,55 @@ struct PlayingFieldView: View {
     ]
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: layout, spacing: 1) {
-                ForEach(data, id: \.self) { item in
-                    VStack {
-                        CardView()
+        VStack{
+            ScrollView {
+                LazyVGrid(columns: layout, spacing: 1) {
+                    ForEach(data, id: \.self) { item in
+                        VStack {
+                            CardView(txt: item, color: Color.white)
+                        }
                     }
                 }
+                .padding(20)
             }
-            .padding(20)
+            
+            HandView(hand: hand)
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        PlayingFieldView()
+        Group {
+            PlayingFieldView()
+        }
+    }
+}
+
+struct HandView: View {
+    var hand: Array<Color>
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(hand[0])
+                .frame(width: 160, height: 80)
+                .padding(20)
+        }
     }
 }
 
 struct CardView: View {
+    var txt: String
+    var color: Color
     var body: some View {
         ZStack {
             Rectangle()
-                .fill(Color.white)
+                .fill(color)
                 .frame(height: 40)
                 .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
             
-            Text("txt")
+            Text(txt)
         }
     }
 }
